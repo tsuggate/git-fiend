06c282e34cba4dc6efba04f0f56f377cf0c287c4
Toby Suggate <t.suggate@adinstruments.com>
2017-10-13 15:33:35 +1300

    Figure out how to log history.

--- src/data/query-repo.ts
+++ src/data/query-repo.ts
@@ -1,14 +1,45 @@
 import * as Git from 'nodegit';
-import {Repository} from 'nodegit';
+import {Commit, Repository} from 'nodegit';
 import * as path from 'path';
+import * as moment from 'moment';
 
 
 const pathToRepo = path.resolve(__dirname, '..');
 
 export async function printQuery() {
    const repo: Repository = await Git.Repository.open(pathToRepo);
-   const commit = await repo.getMasterCommit();
 
-   console.log(commit.message());
-   console.log('hi');
+   const commit: Commit = await repo.getMasterCommit();
+
+   const date = moment(new Date(commit.date())).fromNow();
+
+   console.log(commit.message(), date);
+
+   await walkHistory(commit);
+}
+
+export function walkHistory(commit: Commit) {
+   return new Promise(resolve => {
+      const history = commit.history();
+
+      // History emits "commit" event for each commit in the branch's history
+      history.on("commit", function(commit: any) {
+         console.log("commit " + commit.sha());
+         console.log("Author:", commit.author().name() +
+            " <" + commit.author().email() + ">");
+         console.log("Date:", commit.date());
+         console.log("\n    " + commit.message());
+      });
+
+      history.on('end', () => {
+         resolve();
+      });
+
+      history.on('error', () => {
+         resolve();
+      });
+
+      (history as any).start();
+   })
+
 }
